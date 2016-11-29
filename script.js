c2446af28ff290aa208b30882183ae837176540d
LINK\josh <stetjj01@gmail.com>
2016-11-28 19:51:50 -0500

    created hp functions, added functionality to forest page

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
 var gold;
 var name;
 var char;
+var exp;
+var hp;
 const USER_NAME = "userName";
 const GOLD = "gold";
-const CHARACTER = char;
+const CHARACTER = "char";
+const EXP = "exp";
+const HP = "hp";
 var dataChanged;
 
 function $(a) {
     return document.getElementById(a);
 }
-
 function HiddenChest() {
     var value = RandomInt(0,10);
  $("divOut").innerHTML="You found a chest with " + value + " Gold";
     IncreaseGold(value);
-    SaveResults();
+    SaveGold();
 }
-
 function RandomInt(low, high) {
     if(low <= high) {
         return Math.floor(Math.random()*(high-low+1)) + low;
     }
 }
-
 function IncreaseGold(val){
     gold += val;
     dataChanged = true;
     document.getElementById("divGold").innerHTML = gold;
 }
-
 function DecreaseGold(val){
     gold -= val;
     dataChanged = true;
     document.getElementById("divGold").innerHTML = gold;
 }
-
+function DecreaseHp(val){
+    hp -= val;
+    dataChanged = true;
+    document.getElementById("divHp").innerHTML = hp;
+}
 function RegisterLocal(){
     name = document.getElementById("txtName").value;
 
@@ -48,8 +52,7 @@ function RegisterLocal(){
     localStorage.setItem(USER_NAME, name);
     window.location.href = "index.html";
 }
-
-function SaveResults(){
+function SaveGold(){
 
     if (dataChanged == true){
         localStorage.setItem(GOLD, gold);
@@ -57,7 +60,14 @@ function SaveResults(){
         dataChanged = false;
     }
 }
+function SaveHp(){
 
+    if (dataChanged == true){
+        localStorage.setItem(HP, hp);
+        alert("Your hp has been updated");
+        dataChanged = false;
+    }
+}
 function PageLoad(){
 
     if (localStorage.getItem(USER_NAME) != null){
@@ -76,7 +86,6 @@ function PageLoad(){
 
     document.getElementById("divGold").innerHTML = gold;
 }
-
 function DeleteInformation(){
     localStorage.clear();
     dataChanged = false;
@@ -93,8 +102,8 @@ function SaveCharacter() {
     }else if ($("radMage").checked == true) {
         localStorage.setItem(CHARACTER, "Mage");
     }else {
-        alert("No character selected. Please select a character");
-        window.location.href = "index.html"
+        alert("No character selected. Soldier has been selected for you.");
+        localStorage.setItem(CHARACTER, "Soldier");
     }
 }
 function ChangeLocation() {
@@ -105,7 +114,33 @@ function ChangeLocation() {
     }else if ($("radCave").checked == true){
         window.location.href = "Cave.html"
     }else {
-        alert("No character selected. Please select a character");
-        window.location.href = "index.html"
+        alert("No location selected. Please select a location");
+    }
+}
+function IsDead() {
+    if (hp<1){
+        DeleteInformation();
+    }
+}
+function Field() {
+    window.location.href = "index.html"
+}
+function Forest() {
+    if ($("chkExplore").checked == true){
+        $("divExplore").innerHTML = "";
+    }
+    if ($("chkOpenChest").checked == true){
+        $("divOpenChest").innerHTML = "Obviously there is no treasure in here, that would be too easy. You lose 2 HP arbatraraly";
+        DecreaseHp(2);
+        SaveHp();
+    }
+    if ($("chkSearchCorpse").checked == true){
+        $("divSearchCorpse").innerHTML = "You found rigamortis, -2HP";
+    }
+    if ($("chkTalkAnimal").checked == true){
+        $("divTalkAnimal").innerHTML = "";
+    }
+    if ($("chkClimbTree").checked == true){
+        $("divClimbTree").innerHTML = "";
     }
 }
